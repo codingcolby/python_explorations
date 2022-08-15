fde8d245974797cb8420fb4e055f064fba0e6f75
codingcolby <55331685+codingcolby@users.noreply.github.com>
2022-08-14 18:42:08 -0700

    initial commit of completed exercises

---
+++ jimmyFiveTimes.py
@@ -0,0 +1,13 @@
+#print('My name is ')
+# writing this as a while look instead of a for loop
+#i = 0
+#while i < 5:
+#for i in range(5):
+#    print('Jimmy Five Times ' + str(i))
+#   i = i + 1
+
+#or
+
+print('My name is')
+for i in range(12,16):
+    print('Jimmy Five Times ' + str(i))
