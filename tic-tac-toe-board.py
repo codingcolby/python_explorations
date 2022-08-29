cecfae2857a6ba541fbe91c958ce636fe7a44b3b
codingcolby <55331685+codingcolby@users.noreply.github.com>
2022-08-28 17:42:22 -0700

    initial commit; modeling a tic-tac-toe board from a data structure

---
+++ tic-tac-toe-board.py
@@ -0,0 +1,19 @@
+#tic-tac-toe board-only
+
+theBoard = {'top-L': ' ', 'top-M':' ', 'top-R': ' ', 'mid-L':' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L':' ', 'low-M':' ', 'low-R':' '}
+
+import pprint
+
+pprint.pprint(theBoard)
+
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-----')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-----')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+ 
+print('Here is your board')
+
+printBoard(theBoard)    
